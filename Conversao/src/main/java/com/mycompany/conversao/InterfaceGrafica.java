05de5b42f035d4229a22186b61c6666b6bd1c4f7
Isaac <isaacgames95@gmail.com>
2023-07-13 20:54:04 -0300

    Melhora da UI e adição de algumas funcionalidades

--- Conversao/src/main/java/com/mycompany/conversao/InterfaceGrafica.java
+++ Conversao/src/main/java/com/mycompany/conversao/InterfaceGrafica.java
@@ -17,12 +17,15 @@ public class InterfaceGrafica extends javax.swing.JFrame {
      */
     public InterfaceGrafica() {
         initComponents();
+        valorOrigemFormatadoTF.setBackground(new java.awt.Color(0,0,0,1));
+        resultadoFormatadoTF.setBackground(new java.awt.Color(0,0,0,1));
     }
     
     
     public Moeda moedaOrigem = null;
     public Moeda moedaDestino = null;
     Conversor conversor = new Conversor();
+    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -38,10 +41,15 @@ public class InterfaceGrafica extends javax.swing.JFrame {
         moedaDestinoCB = new javax.swing.JComboBox<>();
         valorTF = new javax.swing.JTextField();
         converterBT = new javax.swing.JButton();
-        resultadoTF1 = new javax.swing.JTextField();
+        resultadoTF = new javax.swing.JTextField();
+        valorOrigemFormatadoTF = new javax.swing.JTextField();
+        resultadoFormatadoTF = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jPanel1.setBackground(new java.awt.Color(48, 49, 52));
+
+        moedaOrigemCB.setFont(new java.awt.Font("SansSerif", 0, 16)); // NOI18N
         moedaOrigemCB.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "<Selecione>", "Real", "Dólar", "Euro" }));
         moedaOrigemCB.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -49,6 +57,7 @@ public class InterfaceGrafica extends javax.swing.JFrame {
             }
         });
 
+        moedaDestinoCB.setFont(new java.awt.Font("SansSerif", 0, 16)); // NOI18N
         moedaDestinoCB.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "<Selecione>", "Real", "Dólar", "Euro" }));
         moedaDestinoCB.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -56,6 +65,8 @@ public class InterfaceGrafica extends javax.swing.JFrame {
             }
         });
 
+        valorTF.setFont(new java.awt.Font("Serif", 0, 16)); // NOI18N
+        valorTF.setForeground(new java.awt.Color(154, 160, 157));
         valorTF.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 valorTFActionPerformed(evt);
@@ -69,41 +80,78 @@ public class InterfaceGrafica extends javax.swing.JFrame {
             }
         });
 
-        resultadoTF1.setEditable(false);
+        resultadoTF.setEditable(false);
+        resultadoTF.setFont(new java.awt.Font("Serif", 0, 16)); // NOI18N
+        resultadoTF.setForeground(new java.awt.Color(154, 160, 157));
+
+        valorOrigemFormatadoTF.setEditable(false);
+        valorOrigemFormatadoTF.setFont(new java.awt.Font("Serif", 0, 16)); // NOI18N
+        valorOrigemFormatadoTF.setForeground(new java.awt.Color(154, 160, 157));
+        valorOrigemFormatadoTF.setBorder(null);
+        valorOrigemFormatadoTF.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                valorOrigemFormatadoTFActionPerformed(evt);
+            }
+        });
+
+        resultadoFormatadoTF.setEditable(false);
+        resultadoFormatadoTF.setFont(new java.awt.Font("Serif", 0, 36)); // NOI18N
+        resultadoFormatadoTF.setForeground(new java.awt.Color(232, 234, 237));
+        resultadoFormatadoTF.setBorder(null);
+        resultadoFormatadoTF.setSelectedTextColor(new java.awt.Color(242, 242, 242));
+        resultadoFormatadoTF.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                resultadoFormatadoTFActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap(148, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(moedaOrigemCB, javax.swing.GroupLayout.PREFERRED_SIZE, 133, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(40, 40, 40)
-                        .addComponent(moedaDestinoCB, javax.swing.GroupLayout.PREFERRED_SIZE, 133, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                        .addComponent(resultadoTF1, javax.swing.GroupLayout.PREFERRED_SIZE, 285, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(valorTF, javax.swing.GroupLayout.PREFERRED_SIZE, 285, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(valorTF)
+                            .addComponent(resultadoTF, javax.swing.GroupLayout.PREFERRED_SIZE, 146, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(moedaDestinoCB, javax.swing.GroupLayout.PREFERRED_SIZE, 133, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(moedaOrigemCB, javax.swing.GroupLayout.PREFERRED_SIZE, 133, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(106, 106, 106)
+                        .addGap(108, 108, 108)
                         .addComponent(converterBT)))
-                .addContainerGap(145, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(77, 77, 77)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(resultadoFormatadoTF)
+                        .addGap(282, 282, 282))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(valorOrigemFormatadoTF, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(398, Short.MAX_VALUE))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap(72, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(moedaOrigemCB, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(moedaDestinoCB, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(46, 46, 46)
+                .addComponent(valorOrigemFormatadoTF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(resultadoFormatadoTF, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(166, 166, 166)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(valorTF, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(moedaOrigemCB, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addComponent(valorTF, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(resultadoTF)
+                    .addComponent(moedaDestinoCB, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(converterBT)
-                .addGap(18, 18, 18)
-                .addComponent(resultadoTF1, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(109, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -120,10 +168,11 @@ public class InterfaceGrafica extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    
     private void converterBTActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_converterBTActionPerformed
-        resultadoTF1.setText("");
+        resultadoTF.setText("");
         validaEntrada(valorTF.getText());
-       
+
         if(moedaOrigemCB.getSelectedItem().toString().equals("<Selecione>")){
             JOptionPane.showMessageDialog(rootPane, "Você deve selecionar uma moeda de origem", "Erro", JOptionPane.ERROR_MESSAGE);
         }
@@ -131,45 +180,15 @@ public class InterfaceGrafica extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(rootPane, "Você deve selecionar uma moeda de destino", "Erro", JOptionPane.ERROR_MESSAGE);
         }
         else{
-            
-            
-            /*if(moedaDestinoCB.getSelectedItem().toString().equals("Real")){
-            moedaDestino = new Real();
-            }
-            if(moedaDestinoCB.getSelectedItem().toString().equals("Dólar")){
-            moedaDestino = new Dolar();
-            }
-            if(moedaDestinoCB.getSelectedItem().toString().equals("Euro ")){
-            moedaDestino = new Euro();
-            }
-            if(moedaDestinoCB.getSelectedItem().toString().equals("<Selecione>")){
-            JOptionPane.showMessageDialog(rootPane, "Você deve selecionar uma moeda", "Selecione uma moeda", JOptionPane.ERROR_MESSAGE);
-            }
-            
-            /////////////////////////////////////////////////////////////////
-            
-            if(moedaOrigemCB.getSelectedItem().toString().equals("Real")){
-            moedaOrigem = new Real();
-            }
-            if(moedaOrigemCB.getSelectedItem().toString().equals("Dólar")){
-            moedaOrigem = new Dolar();
-            }
-            if(moedaOrigemCB.getSelectedItem().toString().equals("Euro ")){
-                moedaOrigem = new Euro();
-            }
-            if(moedaOrigemCB.getSelectedItem().toString().equals("<Selecione>")){
-            JOptionPane.showMessageDialog(rootPane, "Você deve selecionar uma moeda", "Selecione uma moeda", JOptionPane.ERROR_MESSAGE);
-            }*/
-            
-            
-            
             moedaOrigem.setValor(Double.parseDouble(valorTF.getText().replace(',', '.')));
             double resultado = conversor.converter(moedaOrigem, moedaDestino);
-            resultadoTF1.setText(String.valueOf(resultado));
+            resultadoTF.setText(String.valueOf(resultado));
             
-            
+            valorOrigemFormatadoTF.setText(valorTF.getText()+" " + moedaOrigemCB.getSelectedItem().toString() + " igual a ");
+            resultadoFormatadoTF.setText(resultadoTF.getText() + " " + moedaDestinoCB.getSelectedItem().toString());
+
         }
-        
+
     }//GEN-LAST:event_converterBTActionPerformed
 
     private void valorTFActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_valorTFActionPerformed
@@ -177,34 +196,40 @@ public class InterfaceGrafica extends javax.swing.JFrame {
     }//GEN-LAST:event_valorTFActionPerformed
 
     private void moedaDestinoCBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_moedaDestinoCBActionPerformed
-        if(moedaDestinoCB.getSelectedItem().toString().equals("Real")){
-            moedaDestino = new Real();
+        if(moedaDestinoCB.getSelectedItem().toString().equals(moedaOrigemCB.getSelectedItem().toString())){
+            moedaDestinoCB.setSelectedItem("<Selecione>");
         }
-        if(moedaDestinoCB.getSelectedItem().toString().equals("Dólar")){
-            moedaDestino = new Dolar();
-        }
-        if(moedaDestinoCB.getSelectedItem().toString().equals("Euro")){
-            moedaDestino = new Euro();
-        }
-        if(moedaDestinoCB.getSelectedItem().toString().equals("<Selecione>")){
-            JOptionPane.showMessageDialog(rootPane, "Você deve selecionar uma moeda", "Selecione uma moeda", JOptionPane.ERROR_MESSAGE);
+        switch (moedaDestinoCB.getSelectedItem().toString()) {
+            case "Real" -> moedaDestino = new Real();
+            case "Dólar" -> moedaDestino = new Dolar();
+            case "Euro" -> moedaDestino = new Euro();
+            case "<Selecione>" -> JOptionPane.showMessageDialog(rootPane, "Você deve selecionar uma moeda", "Selecione uma moeda", JOptionPane.ERROR_MESSAGE);
+            default -> {
+            }
         }
     }//GEN-LAST:event_moedaDestinoCBActionPerformed
 
     private void moedaOrigemCBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_moedaOrigemCBActionPerformed
-        if(moedaOrigemCB.getSelectedItem().toString().equals("Real")){
-            moedaOrigem = new Real();
+        if(moedaOrigemCB.getSelectedItem().toString().equals(moedaDestinoCB.getSelectedItem().toString())){
+            moedaOrigemCB.setSelectedItem("<Selecione>");
         }
-        if(moedaOrigemCB.getSelectedItem().toString().equals("Dólar")){
-            moedaOrigem = new Dolar();
-        }
-        if(moedaOrigemCB.getSelectedItem().toString().equals("Euro")){
-            moedaOrigem = new Euro();
-        }
-        if(moedaOrigemCB.getSelectedItem().toString().equals("<Selecione>")){
-            JOptionPane.showMessageDialog(rootPane, "Você deve selecionar uma moeda", "Selecione uma moeda", JOptionPane.ERROR_MESSAGE);
+        switch (moedaOrigemCB.getSelectedItem().toString()) {
+            case "Real" -> moedaOrigem = new Real();
+            case "Dólar" -> moedaOrigem = new Dolar();
+            case "Euro" -> moedaOrigem = new Euro();
+            case "<Selecione>" -> JOptionPane.showMessageDialog(rootPane, "Você deve selecionar uma moeda", "Selecione uma moeda", JOptionPane.ERROR_MESSAGE);
+            default -> {
+            }
         }
     }//GEN-LAST:event_moedaOrigemCBActionPerformed
+
+    private void valorOrigemFormatadoTFActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_valorOrigemFormatadoTFActionPerformed
+        
+    }//GEN-LAST:event_valorOrigemFormatadoTFActionPerformed
+
+    private void resultadoFormatadoTFActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_resultadoFormatadoTFActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_resultadoFormatadoTFActionPerformed
     
     private void validaEntrada(String entrada){
         String regex = "^\\d+(?:[,.]\\d+)?$";
@@ -254,7 +279,9 @@ public class InterfaceGrafica extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JComboBox<String> moedaDestinoCB;
     private javax.swing.JComboBox<String> moedaOrigemCB;
-    private javax.swing.JTextField resultadoTF1;
+    private javax.swing.JTextField resultadoFormatadoTF;
+    private javax.swing.JTextField resultadoTF;
+    private javax.swing.JTextField valorOrigemFormatadoTF;
     private javax.swing.JTextField valorTF;
     // End of variables declaration//GEN-END:variables
 }
